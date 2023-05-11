8ce57cf8dbe4f586b6be215fbc0fa522c1f92a40
pollyjhon <joaotv16@gmail.com>
2023-05-11 15:01:31 -0300

    prep2 done

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("what is your first name?");
+        string firstname = Console.ReadLine();
+        Console.WriteLine("what is your second name?");
+        string lastname = Console.ReadLine();
+        Console.WriteLine($"your name is {lastname}, {firstname} {lastname}.");
     }
 }
